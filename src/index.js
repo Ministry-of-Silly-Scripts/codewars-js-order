1b929f81b3dcf2a50d3fbf92aadb43de41678ccf
Marcio Silva <marcio.silva@ibm.com>
2023-07-12 15:19:45 -0300

    added auxiliary function wordPos - return the number embedded in the string as integer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const order = (words) => {
   return words;
 }
 
+const wordPos = (word) => {
+  for (let i = 0; i < word.length; i++) {
+    if (!isNaN(word[i])) {
+      return parseInt(word[i])
+    }
+  }
+
+  return 0;
+}
+
 module.exports = {
   order,
+  wordPos
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const { order } = require("../src/index");
+const { order, wordPos } = require("../src/index");
 
 describe('the order canary spec', () => {
   it('shows the infrastructure works', () => {
@@ -17,5 +17,11 @@ describe('order', () => {
 
   it('should return "Word1 Word2" for input "Word1 Word2"', () => {
     expect(order("Word1 Word2")).toBe("Word1 Word2");
+  });
+});
+
+describe('wordPos', () => {
+  it('it should return the number embedded in the string as integer', () => {
+    expect(wordPos("Word1")).toBe(1);
   });
 });
